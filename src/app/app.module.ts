e38017f73bdf05cd71f08bbffd12f57bfbd4f199
root <eduardadebrum94@gmail.com>
2019-06-02 11:18:51 -0300

    Criação dos componentes necessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteListaComponent } from './cliente-lista/cliente-lista.component';
+import { ClienteNovoComponent } from './cliente-novo/cliente-novo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteListaComponent,
+    ClienteNovoComponent
   ],
   imports: [
     BrowserModule,
